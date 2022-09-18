a28ae9e6e8aa0047e7b22c5139a48f3493674bfb
Naufal Haidir Ridha <naufalhaidirridha@rocketmail.com>
2022-09-18 12:20:58 +0700

    V1

--- routes/web.php
+++ routes/web.php
@@ -6,10 +6,10 @@ Route::get('/help', function () { return view('pages.frontend.main.help'); });
 
 Auth::routes();
 
-include('backend\system\dashboard.php');
-include('backend\system\dummy.php');
-include('backend\system\management.php');
-include('backend\system\help.php');
+// include('backend\system\dashboard.php');
+// include('backend\system\dummy.php');
+// include('backend\system\management.php');
+// include('backend\system\help.php');
 
 Auth::routes();
 
