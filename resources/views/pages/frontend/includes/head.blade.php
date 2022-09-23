d17f04ff873d13923fb98d50cad54ee95e023e0a
Naufal Haidir Ridha <naufalhaidirridha@rocketmail.com>
2022-09-23 22:08:50 +0700

    Update Floating Chat

--- resources/views/pages/frontend/includes/head.blade.php
+++ resources/views/pages/frontend/includes/head.blade.php
@@ -12,4 +12,32 @@
   <script> paceOptions = { elements: true }; </script>
   <script src="/assets/frontend/js/pace.min.js"></script>
   <link id="pagestyle" rel="stylesheet" type="text/css" href="/assets/frontend/css/skin-1.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" />
+
+<!-- add your custom CSS -->
+<style>
+body {
+ font-family: sans-serif;
+}
+
+/* Add WA floating button CSS */
+.floating {
+ position: fixed;
+ width: 60px;
+ height: 60px;
+ bottom: 40px;
+ right: 40px;
+ background-color: #25d366;
+ color: #fff;
+ border-radius: 50px;
+ text-align: center;
+ font-size: 30px;
+ box-shadow: 2px 2px 3px #999;
+ z-index: 100;
+}
+
+.fab-icon {
+ margin-top: 16px;
+}
+</style>
 </head>
