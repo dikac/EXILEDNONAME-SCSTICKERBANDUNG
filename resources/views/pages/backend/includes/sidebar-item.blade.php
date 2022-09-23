cdba9d84c819852a0d87f44216be9ba3d1d14c2f
Naufal Haidir Ridha <naufalhaidirridha@rocketmail.com>
2022-09-23 21:28:48 +0700

    V3

--- resources/views/pages/backend/includes/sidebar-item.blade.php
+++ resources/views/pages/backend/includes/sidebar-item.blade.php
@@ -2,6 +2,6 @@
   <h4 class="menu-text"> MAIN </h4>
   <i class="menu-icon ki ki-bold-more-hor icon-md"></i>
 </li>
-<li class="menu-item {{ (request()->is('dashboard/banners*')) ? 'menu-item-active' : '' }}"><a href="/dashboard/banners" class="menu-link"><i class="menu-icon fas fa-list-ul"></i><span class="menu-text"> Banners </span></a></li>
+<!-- <li class="menu-item {{ (request()->is('dashboard/banners*')) ? 'menu-item-active' : '' }}"><a href="/dashboard/banners" class="menu-link"><i class="menu-icon fas fa-list-ul"></i><span class="menu-text"> Banners </span></a></li> -->
 <li class="menu-item {{ (request()->is('dashboard/products*')) ? 'menu-item-active' : '' }}"><a href="/dashboard/products" class="menu-link"><i class="menu-icon fas fa-list-ul"></i><span class="menu-text"> Products </span></a></li>
 <li class="menu-item {{ (request()->is('dashboard/sliders*')) ? 'menu-item-active' : '' }}"><a href="/dashboard/sliders" class="menu-link"><i class="menu-icon fas fas fa-list-ul"></i><span class="menu-text"> Sliders </span></a></li>
