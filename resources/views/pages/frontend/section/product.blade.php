df978f0fa4b45df9b48716ce528d1f8ca0677e28
Naufal Haidir Ridha <naufalhaidirridha@rocketmail.com>
2022-09-23 21:31:37 +0700

    V6

--- resources/views/pages/frontend/section/product.blade.php
+++ resources/views/pages/frontend/section/product.blade.php
@@ -10,40 +10,39 @@
       @foreach($product as $product)
       <div class="item">
         <div class="product">
-          <a class="add-fav tooltipHere" data-toggle="tooltip" data-original-title="Add to Wishlist"
-          data-placement="left">
-          <i class="glyphicon glyphicon-heart"></i>
-        </a>
-        <div class="image">
-          <div class="quickview">
-            <a data-toggle="modal" class="btn btn-xs btn-quickview" href="#" data-target="#productSetailsModalAjax">Quick View </a>
+          <!-- <a class="add-fav tooltipHere" data-toggle="tooltip" data-original-title="Add to Wishlist" data-placement="left">
+            <i class="glyphicon glyphicon-heart"></i>
+          </a> -->
+          <div class="image">
+            <div class="quickview">
+              <a data-toggle="modal" class="btn btn-xs btn-quickview" href="#" data-target="#productSetailsModalAjax">Quick View </a>
+            </div>
+            <a href="product-details.html"><img src="/files/photo/product/{{ $product->photo }}" alt="img"></a>
+            <div class="promotion"><span class="new-product">  ⭐ POPULAR </span></div>
           </div>
-          <a href="product-details.html"><img src="/files/photo/product/{{ $product->photo }}" alt="img"></a>
-          <div class="promotion"><span class="new-product">  ⭐ POPULAR </span></div>
-        </div>
-        <div class="description">
-          <h4><a href="product-details.html"> {{ $product->name }} </a><hr></h4>
-          <p>
-            <b> -- Description -- </b><br>
-            {{ $product->description }}
-          </p>
-        </div>
-        <div class="action-control">
+          <div class="description">
+            <h4><a href="product-details.html"> {{ $product->name }} </a><hr></h4>
+            <p>
+              <b> -- Description -- </b><br>
+              {{ $product->description }}
+            </p>
+          </div>
+          <div class="action-control">
 
-          <a href="https://api.whatsapp.com/send?phone=%20{{ $setting->phone }}&text=Pesan ini tersambung otomatis dari produk di website, Saya Ingin Menanyakan/Membeli Item ini: %0A({{ $product->name }} - {{ $product->description }})%0ATerima%20Kasih%20--" target="_blank" class="btn btn-warning">
-            <span class="add2cart">
-              <i class="glyphicon glyphicon-shopping-cart"></i> READY </span>
-          </a>
+            <a href="https://api.whatsapp.com/send?phone=%20{{ $setting->phone }}&text=Pesan ini tersambung otomatis dari produk di website, Saya Ingin Menanyakan/Membeli Item ini: %0A({{ $product->name }} - {{ $product->description }})%0ATerima%20Kasih%20--" target="_blank" class="btn btn-warning">
+              <span class="add2cart">
+                <i class="glyphicon glyphicon-shopping-cart"></i> READY </span>
+              </a>
+            </div>
+          </div>
         </div>
+        @endforeach
+
       </div>
+
+      <hr>
+      <center><a href="#" class="btn btn-inverse"> Lihat Semua Produk </a></center>
+      <hr>
+
     </div>
-    @endforeach
-
   </div>
-
-  <hr>
-  <center><a href="#" class="btn btn-inverse"> Lihat Semua Produk </a></center>
-  <hr>
-
-</div>
-</div>
