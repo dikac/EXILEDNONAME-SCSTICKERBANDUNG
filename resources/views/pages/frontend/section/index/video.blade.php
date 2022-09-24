4f5d1209b2ca954aac4dd242817ea843c6c7f93a
Naufal Haidir Ridha <naufalhaidirridha@rocketmail.com>
2022-09-24 14:06:44 +0700

    Update Video

--- resources/views/pages/frontend/section/index/video.blade.php
+++ resources/views/pages/frontend/section/index/video.blade.php
@@ -12,7 +12,7 @@
         </video>
         <h3>
           <span> Indomaret </span>
-          Video kegiatan pengerjaan project Indomaret
+          Video kegiatan pengerjaan Cutting Indomaret
         </h3>
       </div>
     </div>
@@ -20,15 +20,15 @@
     <div class="col-md-4 col-sm-4 col-xs-12">
       <div class="inner">
         <video width="100%" class="img-responsive" controls>
-          <source src="/files/video/indomaret/indomaret_1.mp4" type="video/mp4">
+          <source src="/files/video/pertashop/pertashop_1.mp4" type="video/mp4">
             <source src="movie.ogg" type="video/ogg">
               Your browser does not support the video tag.
             </source>
           </source>
         </video>
         <h3>
-          <span> Indomaret </span>
-          Video kegiatan pengerjaan project Indomaret
+          <span> Pertashop </span>
+          Video kegiatan pengerjaan Cutting Pertashop
         </h3>
       </div>
     </div>
@@ -44,10 +44,11 @@
         </video>
         <h3>
           <span> Safety Sign </span>
-          Video kegiatan pengerjaan project Safety Sign
+          Video kegiatan pengerjaan Safety Sign
         </h3>
       </div>
     </div>
+
   </div>
 </div>
 <br>
