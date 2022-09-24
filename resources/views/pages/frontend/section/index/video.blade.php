8d9577001984151dc0767b2116fbd440afa40943
Naufal Haidir Ridha <naufalhaidirridha@rocketmail.com>
2022-09-24 14:04:05 +0700

    Update Video

--- resources/views/pages/frontend/section/index/video.blade.php
+++ resources/views/pages/frontend/section/index/video.blade.php
@@ -1,7 +1,7 @@
 <div class="container">
   <div class="row featuredImgLook2">
 
-    <div class="col-md-6 col-sm-12 col-xs-12">
+    <div class="col-md-4 col-sm-4 col-xs-12">
       <div class="inner">
         <video width="100%" class="img-responsive" controls>
           <source src="/files/video/indomaret/indomaret_1.mp4" type="video/mp4">
@@ -17,18 +17,34 @@
       </div>
     </div>
 
-    <div class="col-md-6 col-sm-12 col-xs-12">
+    <div class="col-md-4 col-sm-4 col-xs-12">
       <div class="inner">
         <video width="100%" class="img-responsive" controls>
-          <source src="/files/video/pertashop/pertashop_1.mp4" type="video/mp4">
+          <source src="/files/video/indomaret/indomaret_1.mp4" type="video/mp4">
             <source src="movie.ogg" type="video/ogg">
               Your browser does not support the video tag.
             </source>
           </source>
         </video>
         <h3>
-          <span> Pertashop </span>
-          Video kegiatan pengerjaan project Pertashop
+          <span> Indomaret </span>
+          Video kegiatan pengerjaan project Indomaret
+        </h3>
+      </div>
+    </div>
+
+    <div class="col-md-4 col-sm-4 col-xs-12">
+      <div class="inner">
+        <video width="100%" class="img-responsive" controls>
+          <source src="/files/video/safety_sign/safety_sign_1.mp4" type="video/mp4">
+            <source src="movie.ogg" type="video/ogg">
+              Your browser does not support the video tag.
+            </source>
+          </source>
+        </video>
+        <h3>
+          <span> Safety Sign </span>
+          Video kegiatan pengerjaan project Safety Sign
         </h3>
       </div>
     </div>
